5ac89b818d70816b4883faac6e181bb60f9cd55a
juan1luis <saenzjuanluis334@gmail.com>
2022-12-02 13:02:09 -0600

    jol

---
+++ neflixreto.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+nameC = "movies.csv"
+nrowsC = 1000
+
+sidebar = st.sidebar
+titleS = 'Buscar peliculas'
+descript = '¡Busca peliculas usando el filtro que prefieras!'
+
+DesFil1 = 'Buscar filmes por tituulo'
+DesFil2 = 'Buscar filmes por director'
+
+@st.cache(suppress_st_warning=True)
+def showDataset(name,nrows):
+    df=pd.read_csv(name,nrows=nrows)
+    dfvis = df.copy()
+    return df, dfvis
+
+df, dfvis= showDataset(nameC,nrowsC)
+
+
+def filter_data_by_filme(filme):
+    filtered_data_filme = df[df['name'].str.lower().str.contains(filme)]
+    return filtered_data_filme
+
+def filter_data_by_director(director):
+    filtered_data_filme = df[df['director'].str.lower().str.contains(director)]
+    return filtered_data_filme
+
+sidebar.title(titleS)
+sidebar.header(descript)
+
+
+titmov = sidebar.text_input(DesFil1)
+if sidebar.button('Buscar'):
+    dfvis = (filter_data_by_filme(str(titmov).lower()))
+
+dirct_name = sidebar.selectbox(DesFil2,df['director'].unique())
+if sidebar.button('Buscar Director'):
+    dfvis = (filter_data_by_director(str(dirct_name).lower()))
+
+if sidebar.checkbox('Visualizar Dataset?'):
+    st.dataframe(dfvis)
+    if dfvis.shape[0] == df.shape[0]:
+        st.text('Sin filtros')
+    else:
+        st.text(f'Total de peliculas filtradas: {dfvis.shape[0]}')
+
+
+
+
+
